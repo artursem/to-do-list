7f6e6fbdd502bc88e67ec0e9122265b7b9f673e9
artursem <artur.semeniuk@hmail.com>
2021-04-08 20:22:19 +0200

    js tweaks

--- index.js
+++ index.js
@@ -5,35 +5,84 @@ class ToDoList {
         } else {
             this.tasks = [];
         };
+        this.counter = 0;
     };
     addItems = (e) => {
         e.preventDefault();
         if(itemInput.value==false) {
             alert('please enter value');
         } else {
-            this.tasks.push(itemInput.value);
-            this.updateList();
-            itemInput.value = '';
+            let taskContent = itemInput.value;
+            let newItem = new ListItem(this.counter, taskContent);
+
+            this.tasks.push(newItem); // update js list
+
+            this.updateDomList();  // update DOM list
+            
+            itemInput.value = ''; //clear the input form
+            this.counter++; // counter up!
         }
     };
-    updateList = () => {
-        let newLi = document.createElement('li');
-        let newDiv = document.createElement('div');
-        
-        newDiv.innerHTML += `<input type="checkbox">`;
-        newDiv.innerHTML += itemInput.value;
-        newDiv.innerHTML += `<button>x</button>`;
 
+    removeAllLis = () => {
+        let allLis = [].slice.call(ul.children);
+        for (let i in allLis) {
+            allLis[i].remove();
+        }
+    };
+    
+    removeLi = (li) => {
+        let selected = this.tasks.find(val => val.id === li);
+        // console.log(selected.id);
 
-        newLi.appendChild(newDiv);
-        ul.appendChild(newLi);
-        console.log(this.tasks);
+        // REMOVE FROM JS
+        this.tasks.splice(selected.id, 1);
+
+        // UPDATE DOM
+        this.updateDomList();
+    };
+
+    updateDomList = () => {
+        // REMOVE ALL LIS @ DOM
+        this.removeAllLis();
+
+        // ADD ALL ITEMS
+        for (let i in this.tasks) {
+            let newLi = document.createElement('li');
+            let newDiv = document.createElement('div');
+
+            newDiv.innerHTML += `<input type="checkbox" id="check${i}">`; // checked
+            newDiv.innerHTML += this.tasks[i].taskContent;
+            newDiv.innerHTML += `<button class="trashBtn" id="trash${i}">${trashIcon}</button>`;
+
+            newLi.appendChild(newDiv);
+            ul.appendChild(newLi);
+        };
+
+        // TRASH BUTTONS
+        let trashBtnAll = document.querySelectorAll('.trashBtn');
+        trashBtnAll.forEach((button, nr) => {
+            button.addEventListener('click', () => {
+                console.log('clicked ', nr);
+                
+                this.removeLi(nr);
+            });
+        });
+    };
+};
+
+class ListItem {
+    constructor(id, taskContent) {
+        this.id = id;
+        this.done = false;
+        this.taskContent = taskContent;
     }
-
 }
+
 
 const toDo = new ToDoList;
 
+const trashIcon = '<i class="fas fa-trash"></i>';
 const ul = document.querySelector('#ul');
 const itemInput = document.querySelector('#itemInput');
 const submit = document.querySelector('#submit');
